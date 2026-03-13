1cd8ae29502791ca1ec1c91cc6699589133274f2
AkshayAghera <akshayaghera85@gmail.com>
2026-03-13 10:49:24 +0530

    Ready for cloud build

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'ems-cache-v7'; // 👈 CHANGE NUMBER EVERY DEPLOY
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => {
+          if (k !== CACHE_NAME) return caches.delete(k);
+        })
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
